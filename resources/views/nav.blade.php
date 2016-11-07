dac8eb85943caef7387cc4f4358936c6d0e8ba00
evansmakamu <makamuevans@gmail.com>
2016-11-07 19:06:44 +0300

    web separated

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,47 @@
+<nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">Sentimental Energy</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#profile">About Us</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#values">Our Core Values</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#products">Products and Services</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#contact">Contact Us</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        Solar Explained <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu" role="menu" style="background-color: blue">
+                        <li class="pull-right athe">
+                            <a href="{{url('')}}">How Solar Works</a>
+                            <a href="{{url('')}}">The PV Model</a>
+                            <a href="{{url('')}}">Why Go Solar</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('body');
 });
 
 Route::any('sendmail', 'MailController@mail');
