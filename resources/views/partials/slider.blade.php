2fa9ed2693766501db1b64c3a3d5f6edae45254e
makamuevans <makamuevans@gmail.com>
2017-01-23 14:06:19 +0300

    v2

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,21 +1,21 @@
 <div id="main-slider" class="main-slider flexslider">
     <ul class="slides">
         <li>
-            <img src="img/solar.jpg" alt="" />
+            <img src="{{url('img/green.jpg')}}" alt="" />
             <div class="flex-caption">
                 <h3>Cheap Solar</h3>
                 <p>We Provode Cheap, Clean And Reliable Solar Energy to All</p>
             </div>
         </li>
         <li>
-            <img src="img/slides/flexslider/2.jpg" alt="" />
+            <img src="{{url('img/solar.jpg')}}" alt="" />
             <div class="flex-caption">
                 <h3>Professionalism</h3>
                 <p>We ara a Duly Registered and Accredited Company in Kenya. Being Professional is our Expertese</p>
             </div>
         </li>
         <li>
-            <img src="img/slides/flexslider/3.jpg" alt="" />
+            <img src="{{url('img/customer.jpg')}}" alt="" />
             <div class="flex-caption">
                 <h3>Get in Touch</h3>
                 <p>Have a Question, Requesting our quotes?. Talk to us</p>
